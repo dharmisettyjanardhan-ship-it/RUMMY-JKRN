ad14b7d6e65e9d880c100dee51c4bfa9b9eb8146
dharmisettyjanardhan-ship-it <dharmisetty.janardhan@gmail.com>
2026-09-17 14:41:20 +0530

    Refactor static file serving in server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const POOL_LIMIT = 201;
 const MAX_PLAYERS = 6;
 const RECONNECT_GRACE_MS = 120000;
 
-const ROOT_DIR = path.join(__dirname, '..');
-const INDEX_FILE = path.join(ROOT_DIR, 'index.html');
+app.use(express.static(__dirname));
 
-app.use(express.static(ROOT_DIR));
-app.get('/', (req,res)=>res.sendFile(INDEX_FILE));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const suits = [
   {s:'♥',c:'red'}, {s:'♦',c:'red'}, {s:'♣',c:'black'}, {s:'♠',c:'black'}
